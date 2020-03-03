cf2a378ae571b1cf3d5351197e47ec3ce276934f
John Codeos <johncodeos@gmail.com>
2020-03-04 00:05:10 +0200

    AsyncTasks replaced with Coroutines

--- app/src/main/java/com/example/linkedinsigninexample/MainActivity.kt
+++ app/src/main/java/com/example/linkedinsigninexample/MainActivity.kt
@@ -5,7 +5,6 @@ import android.annotation.TargetApi
 import android.app.Dialog
 import android.content.Intent
 import android.net.Uri
-import android.os.AsyncTask
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -16,11 +15,14 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.linkedinsigninexample.model.LinkedInEmailModel
 import com.example.linkedinsigninexample.model.LinkedInProfileModel
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import kotlinx.serialization.json.Json
 import org.json.JSONObject
 import org.json.JSONTokener
 import java.io.OutputStreamWriter
-import java.lang.ref.WeakReference
 import java.net.URL
 import java.util.concurrent.TimeUnit
 import javax.net.ssl.HttpsURLConnection
@@ -33,12 +35,12 @@ class MainActivity : AppCompatActivity() {
     lateinit var linkedinCode: String
 
 
-    var linkedInId = ""
-    var linkedInFirstName = ""
-    var linkedInLastName = ""
-    var linkedInEmail = ""
-    var linkedInProfilePicURL = ""
-    var linkedInAccessToken = ""
+    var id = ""
+    var firstName = ""
+    var lastName = ""
+    var email = ""
+    var profilePicURL = ""
+    var accessToken = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -79,11 +81,11 @@ class MainActivity : AppCompatActivity() {
             view: WebView?,
             request: WebResourceRequest?
         ): Boolean {
-            if (request!!.url.toString().startsWith(LinkedInConstants.REDIRECT_URI)) {
-                handleUrl(request.url.toString())
+            if (request?.url.toString().startsWith(LinkedInConstants.REDIRECT_URI)) {
+                handleUrl(request?.url.toString())
 
                 // Close the dialog after getting the authorization code
-                if (request.url.toString().contains("?code=")) {
+                if (request?.url.toString().contains("?code=")) {
                     linkedIndialog.dismiss()
                 }
                 return true
@@ -111,7 +113,7 @@ class MainActivity : AppCompatActivity() {
 
             if (url.contains("code")) {
                 linkedinCode = uri.getQueryParameter("code") ?: ""
-                LinkedinRequestForAccessToken(this@MainActivity, linkedinCode).execute()
+                linkedInRequestForAccessToken()
             } else if (url.contains("error")) {
                 val error = uri.getQueryParameter("error") ?: ""
                 Log.e("Error: ", error)
@@ -120,190 +122,130 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private class LinkedinRequestForAccessToken
-    internal constructor(context: MainActivity, authCode: String) :
-        AsyncTask<Void, Void, String>() {
-
-        private val activityReference: WeakReference<MainActivity> = WeakReference(context)
-
-        var code = ""
-        val grantType = "authorization_code"
-
-        init {
-            this.code = authCode
-        }
-
-        val postParams =
-            "grant_type=" + grantType + "&code=" + code + "&redirect_uri=" + LinkedInConstants.REDIRECT_URI + "&client_id=" + LinkedInConstants.CLIENT_ID + "&client_secret=" + LinkedInConstants.CLIENT_SECRET
-
-
-        override fun doInBackground(vararg params: Void): String {
-            try {
-                val url = URL(LinkedInConstants.TOKENURL)
-                val httpsURLConnection = url.openConnection() as HttpsURLConnection
-                httpsURLConnection.requestMethod = "POST"
-                httpsURLConnection.setRequestProperty(
-                    "Content-Type",
-                    "application/x-www-form-urlencoded"
-                );
-                httpsURLConnection.doInput = true
-                httpsURLConnection.doOutput = true
-                val outputStreamWriter = OutputStreamWriter(httpsURLConnection.outputStream)
+    fun linkedInRequestForAccessToken() {
+        GlobalScope.launch(Dispatchers.Default) {
+            val grantType = "authorization_code"
+            val postParams =
+                "grant_type=" + grantType + "&code=" + linkedinCode + "&redirect_uri=" + LinkedInConstants.REDIRECT_URI + "&client_id=" + LinkedInConstants.CLIENT_ID + "&client_secret=" + LinkedInConstants.CLIENT_SECRET
+            val url = URL(LinkedInConstants.TOKENURL)
+            val httpsURLConnection = url.openConnection() as HttpsURLConnection
+            httpsURLConnection.requestMethod = "POST"
+            httpsURLConnection.setRequestProperty(
+                "Content-Type",
+                "application/x-www-form-urlencoded"
+            )
+            httpsURLConnection.doInput = true
+            httpsURLConnection.doOutput = true
+            val outputStreamWriter = OutputStreamWriter(httpsURLConnection.outputStream)
+            withContext(Dispatchers.IO) {
                 outputStreamWriter.write(postParams)
                 outputStreamWriter.flush()
-                val response = httpsURLConnection.inputStream.bufferedReader()
-                    .use { it.readText() }  // defaults to UTF-8
-                val jsonObject = JSONTokener(response).nextValue() as JSONObject
-
-                val accessToken = jsonObject.getString("access_token") //The access token
-                Log.d("accessToken is: ", accessToken)
-
-                val expiresIn = jsonObject.getInt("expires_in") //When the access token expires
-                Log.d("expires in: ", expiresIn.toString())
-
-                return accessToken
-            } catch (e: Exception) {
-                e.printStackTrace()
-                return ""
             }
-        }
+            val response = httpsURLConnection.inputStream.bufferedReader()
+                .use { it.readText() }  // defaults to UTF-8
+            val jsonObject = JSONTokener(response).nextValue() as JSONObject
 
-        override fun onPostExecute(result: String) {
-            // get a reference to the activity if it is still there
-            val activity = activityReference.get()
-            if (activity == null || activity.isFinishing) return
+            val accessToken = jsonObject.getString("access_token") //The access token
+            Log.d("accessToken is: ", accessToken)
 
-            // Get user's id, first name, last name, profile pic url
-            FetchLinkedInUserProfile(activity, result).execute()
+            val expiresIn = jsonObject.getInt("expires_in") //When the access token expires
+            Log.d("expires in: ", expiresIn.toString())
+
+
+            withContext(Dispatchers.Main) {
+                // Get user's id, first name, last name, profile pic url
+                fetchlinkedInUserProfile(accessToken)
+            }
         }
     }
 
-    private class FetchLinkedInUserProfile
-    internal constructor(context: MainActivity, accessToken: String) :
-        AsyncTask<Void, Void, LinkedInProfileModel?>() {
+    fun fetchlinkedInUserProfile(token: String) {
+        GlobalScope.launch(Dispatchers.Default) {
+            val tokenURLFull =
+                "https://api.linkedin.com/v2/me?projection=(id,firstName,lastName,profilePicture(displayImage~:playableStreams))&oauth2_access_token=$token"
+            val url = URL(tokenURLFull)
+            val httpsURLConnection = url.openConnection() as HttpsURLConnection
+            httpsURLConnection.requestMethod = "GET"
+            httpsURLConnection.doInput = true
+            httpsURLConnection.doOutput = false
+            val response = httpsURLConnection.inputStream.bufferedReader()
+                .use { it.readText() }  // defaults to UTF-8
+            val linkedInProfileModel =
+                Json.nonstrict.parse(LinkedInProfileModel.serializer(), response)
+            withContext(Dispatchers.Main) {
+                Log.d("LinkedIn Access Token: ", token)
+                accessToken = token
 
-        private val activityReference: WeakReference<MainActivity> = WeakReference(context)
+                // LinkedIn Id
+                val linkedinId = linkedInProfileModel.id
+                Log.d("LinkedIn Id: ", linkedinId)
+                id = linkedinId
 
-        var token = ""
+                // LinkedIn First Name
+                val linkedinFirstName = linkedInProfileModel.firstName.localized.enUS
+                Log.d("LinkedIn First Name: ", linkedinFirstName)
+                firstName = linkedinFirstName
 
-        init {
-            this.token = accessToken
-        }
+                // LinkedIn Last Name
+                val linkedinLastName = linkedInProfileModel.lastName.localized.enUS
+                Log.d("LinkedIn Last Name: ", linkedinLastName)
+                lastName = linkedinLastName
 
-        val tokenURLFull =
-            "https://api.linkedin.com/v2/me?projection=(id,firstName,lastName,profilePicture(displayImage~:playableStreams))&oauth2_access_token=$token"
+                // LinkedIn Profile Picture URL
+                /*
+                     Change row of the 'elements' array to get diffrent size of the profile pic
+                     elements[0] = 100x100
+                     elements[1] = 200x200
+                     elements[2] = 400x400
+                     elements[3] = 800x800
+                */
 
-        override fun doInBackground(vararg params: Void): LinkedInProfileModel? {
-            try {
-                val url = URL(tokenURLFull)
-                val httpsURLConnection = url.openConnection() as HttpsURLConnection
-                httpsURLConnection.requestMethod = "GET"
-                httpsURLConnection.doInput = true
-                httpsURLConnection.doOutput = false
-                val response = httpsURLConnection.inputStream.bufferedReader()
-                    .use { it.readText() }  // defaults to UTF-8
-                val linkedInProfileModel =
-                    Json.nonstrict.parse(LinkedInProfileModel.serializer(), response)
-                return linkedInProfileModel
-            } catch (e: Exception) {
-                e.printStackTrace()
-                return null
+                val linkedinProfilePic =
+                    linkedInProfileModel.profilePicture.displayImage.elements.get(2)
+                        .identifiers.get(0).identifier
+                Log.d("LinkedIn Profile URL: ", linkedinProfilePic)
+                profilePicURL = linkedinProfilePic
+
+                // Get user's email address
+                fetchLinkedInEmailAddress(token)
             }
-        }
-
-        override fun onPostExecute(model: LinkedInProfileModel?) {
-            // get a reference to the activity if it is still there
-            val activity = activityReference.get()
-            if (activity == null || activity.isFinishing) return
-            Log.d("LinkedIn Access Token: ", token)
-            activity.linkedInAccessToken = token
-
-            // LinkedIn Id
-            val linkedinId = model?.id
-            Log.d("LinkedIn Id: ", linkedinId ?: "")
-            activity.linkedInId = linkedinId ?: ""
-
-            // LinkedIn First Name
-            val linkedinFirstName = model?.firstName?.localized?.enUS
-            Log.d("LinkedIn First Name: ", linkedinFirstName ?: "")
-            activity.linkedInFirstName = linkedinFirstName ?: ""
-
-            // LinkedIn Last Name
-            val linkedinLastName = model?.lastName?.localized?.enUS
-            Log.d("LinkedIn Last Name: ", linkedinLastName ?: "")
-            activity.linkedInLastName = linkedinLastName ?: ""
-
-            // LinkedIn Profile Picture URL
-            /*
-                 Change row of the 'elements' array to get diffrent size of the profile pic
-                 elements[0] = 100x100
-                 elements[1] = 200x200
-                 elements[2] = 400x400
-                 elements[3] = 800x800
-            */
-
-            val linkedinProfilePic =
-                model?.profilePicture?.displayImage?.elements?.get(2)?.identifiers?.get(0)
-                    ?.identifier
-            Log.d("LinkedIn Profile URL: ", linkedinProfilePic ?: "Not exists")
-            activity.linkedInProfilePicURL = linkedinProfilePic ?: ""
-
-            // Get user's email address
-            FetchLinkedInEmailAddress(activity, token).execute()
         }
     }
 
-
-    private class FetchLinkedInEmailAddress
-    internal constructor(context: MainActivity, accessToken: String) :
-        AsyncTask<Void, Void, LinkedInEmailModel?>() {
-        private val activityReference: WeakReference<MainActivity> = WeakReference(context)
-
-        var token = ""
-
-        init {
-            this.token = accessToken
-        }
-
+    fun fetchLinkedInEmailAddress(token: String) {
         val tokenURLFull =
             "https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))&oauth2_access_token=$token"
 
-        override fun doInBackground(vararg params: Void): LinkedInEmailModel? {
-            try {
-                val url = URL(tokenURLFull)
-                val httpsURLConnection = url.openConnection() as HttpsURLConnection
-                httpsURLConnection.requestMethod = "GET"
-                httpsURLConnection.doInput = true
-                httpsURLConnection.doOutput = false
-                val response = httpsURLConnection.inputStream.bufferedReader()
-                    .use { it.readText() }  // defaults to UTF-8
-                val linkedInProfileModel =
-                    Json.nonstrict.parse(LinkedInEmailModel.serializer(), response)
-                return linkedInProfileModel
-            } catch (e: Exception) {
-                e.printStackTrace()
-                return null
+        GlobalScope.launch(Dispatchers.Default) {
+            val url = URL(tokenURLFull)
+            val httpsURLConnection = url.openConnection() as HttpsURLConnection
+            httpsURLConnection.requestMethod = "GET"
+            httpsURLConnection.doInput = true
+            httpsURLConnection.doOutput = false
+            val response = httpsURLConnection.inputStream.bufferedReader()
+                .use { it.readText() }  // defaults to UTF-8
+            val linkedInProfileModel =
+                Json.nonstrict.parse(LinkedInEmailModel.serializer(), response)
+            withContext(Dispatchers.Main) {
+                // LinkedIn Email
+                val linkedinEmail = linkedInProfileModel.elements.get(0).elementHandle.emailAddress
+                Log.d("LinkedIn Email: ", linkedinEmail)
+                email = linkedinEmail
+
+                openDetailsActivity()
             }
         }
-
-        override fun onPostExecute(model: LinkedInEmailModel?) {
-            // get a reference to the activity if it is still there
-            val activity = activityReference.get()
-            if (activity == null || activity.isFinishing) return
-
-            // LinkedIn Email
-            val linkedinEmail = model?.elements?.get(0)?.elementHandle?.emailAddress
-            Log.d("LinkedIn Email: ", linkedinEmail ?: "")
-            activity.linkedInEmail = linkedinEmail ?: ""
-
-            val myIntent = Intent(activity, DetailsActivity::class.java)
-            myIntent.putExtra("linkedin_id", activity.linkedInId)
-            myIntent.putExtra("linkedin_first_name", activity.linkedInFirstName)
-            myIntent.putExtra("linkedin_last_name", activity.linkedInLastName)
-            myIntent.putExtra("linkedin_email", activity.linkedInEmail)
-            myIntent.putExtra("linkedin_profile_pic_url", activity.linkedInProfilePicURL)
-            myIntent.putExtra("linkedin_access_token", activity.linkedInAccessToken)
-            activity.startActivity(myIntent)
-        }
     }
+
+    fun openDetailsActivity() {
+        val myIntent = Intent(baseContext, DetailsActivity::class.java)
+        myIntent.putExtra("linkedin_id", id)
+        myIntent.putExtra("linkedin_first_name", firstName)
+        myIntent.putExtra("linkedin_last_name", lastName)
+        myIntent.putExtra("linkedin_email", email)
+        myIntent.putExtra("linkedin_profile_pic_url", profilePicURL)
+        myIntent.putExtra("linkedin_access_token", accessToken)
+        startActivity(myIntent)
+    }
+
 }
